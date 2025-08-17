a8fb2577a4bcafb097a29c5810321895c1480cee
warintorn <warintorn0987716154@gmail.com>
2025-08-18 02:49:59 +0700

    New

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio-new/',   // 👈 ต้องใส่ชื่อ repo ตรงนี้
 })
