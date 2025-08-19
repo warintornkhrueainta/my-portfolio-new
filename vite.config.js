2dc9c268ac2f54485ae5e859af5d9117002cb8c7
warintorn <warintorn0987716154@gmail.com>
2025-08-20 03:12:36 +0700

    fix: remove duplicate base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  base: "/"   // 👈 ต้องใส่ชื่อ repo ตรงนี้
+  //base: '/my-portfolio-new/',
+  base: "/",   
 })
+
